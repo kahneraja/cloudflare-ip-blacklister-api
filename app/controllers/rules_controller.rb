d93d19dc7035ab50a61163c155b5da698b309d5e
kahneraja <kahneraja@gmail.com>
2018-05-26 07:23:35 -0400

    wip

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -8,5 +8,14 @@ class RulesController < ApplicationController
     render json: response['result']
   end
 
+  def create
+    email = request.headers['HTTP_EMAIL']
+    key = request.headers['HTTP_KEY']
+    zone_id = request.headers['HTTP_ZONE_ID']
+    body = params['rule'].to_json
+    puts body
+    response = CloudflareGateway.create_block_rule(key, email, zone_id, body)
+    render json: response
+  end
 
 end
--- app/gateways/cloudflare_gateway.rb
+++ app/gateways/cloudflare_gateway.rb
@@ -3,19 +3,21 @@ class CloudflareGateway
   def self.get_zones(key, email)
     url = "https://api.cloudflare.com/client/v4/zones"
     headers = auth_headers(key, email)
-    puts headers
     response = HTTParty.get(url, headers: headers).response
     JSON.parse(response.body)
   end
 
   def self.get_rules(key, email, zone_id)
-    puts key
-    puts email
-    puts zone_id
     url = "https://api.cloudflare.com/client/v4/zones/#{zone_id}/firewall/access_rules/rules?configuration.target=ip"
     headers = auth_headers(key, email)
-    puts headers
     response = HTTParty.get(url, headers: headers).response
+    JSON.parse(response.body)
+  end
+
+  def self.create_block_rule(key, email, zone_id, body)
+    url = "https://api.cloudflare.com/client/v4/zones/#{zone_id}/firewall/access_rules/rules"
+    headers = auth_headers(key, email)
+    response = HTTParty.post(url, body: body, headers: headers).response
     JSON.parse(response.body)
   end
 
