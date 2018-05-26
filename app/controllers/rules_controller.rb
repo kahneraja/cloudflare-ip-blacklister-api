deba5693ec95d4f411bb976f906ee8859c1344ae
kahneraja <kahneraja@gmail.com>
2018-05-26 07:59:29 -0400

    wip

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,11 +1,13 @@
+require 'json'
+
 class RulesController < ApplicationController
 
   def index
     email = request.headers['HTTP_EMAIL']
     key = request.headers['HTTP_KEY']
     zone_id = request.headers['HTTP_ZONE_ID']
-    response = CloudflareGateway.get_rules(key, email, zone_id)
-    render json: response['result']
+    rules = CloudflareGateway.get_all_rules(key, email, zone_id)
+    render json: rules
   end
 
   def create
--- app/gateways/cloudflare_gateway.rb
+++ app/gateways/cloudflare_gateway.rb
@@ -7,8 +7,24 @@ class CloudflareGateway
     JSON.parse(response.body)
   end
 
-  def self.get_rules(key, email, zone_id)
-    url = "https://api.cloudflare.com/client/v4/zones/#{zone_id}/firewall/access_rules/rules?configuration.target=ip"
+  def self.get_all_rules(key, email, zone_id)
+    page = 1
+    response = get_rules(key, email, zone_id, page)
+    page = response['result_info']['page'].to_i
+    total_pages = response['result_info']['total_pages'].to_i
+    data = response['result']
+    while (page < total_pages)
+      page += 1
+      page_response = get_rules(key, email, zone_id, page)
+      page_data = page_response['result']
+      data.concat page_data
+    end
+    data
+  end
+
+  def self.get_rules(key, email, zone_id, page=1)
+    url = "https://api.cloudflare.com/client/v4/zones/#{zone_id}/firewall/access_rules/rules?configuration.target=ip&page=#{page}"
+    puts url
     headers = auth_headers(key, email)
     response = HTTParty.get(url, headers: headers).response
     JSON.parse(response.body)
