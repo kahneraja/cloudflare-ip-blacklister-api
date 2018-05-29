4982873e96f19ecaafd7b9fadab1cd6017ea1345
Kahne Raja <kahneraja@gmail.com>
2018-05-29 16:57:33 -0400

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :zones
-  resources :accounts
-  resources :rules
+  scope '/api' do
+    resources :zones
+    resources :accounts
+    resources :rules
+  end
 end
